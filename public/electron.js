c5cbecfc976302d12ff38572eae979379e0d91dc
appshare1603 <dirk.rost@gmail.com>
2024-12-12 17:02:31 +0100

    dashboard funktioniert jetzt

--- public/electron.js
+++ public/electron.js
@@ -2,6 +2,13 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const isDev = require('electron-is-dev');
 
+// Handle creating/removing shortcuts on Windows when installing/uninstalling
+/*
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+  */
+
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
@@ -9,36 +16,56 @@ function createWindow() {
     height: 800,
     webPreferences: {
       nodeIntegration: true,
+      enableRemoteModule: true,
       contextIsolation: false
     }
   });
 
-  // Load the index.html from a url
-  mainWindow.loadURL(
-    isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
-  );
+  // Determine the correct path to index.html
+  const indexPath = isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../build/index.html')}`;
 
-  // Open the DevTools in development mode
+  console.log('App starting...');
+  console.log('isDev:', isDev);
+  console.log('indexPath:', indexPath);
+  console.log('__dirname:', __dirname);
+  console.log('Resource path:', process.resourcesPath);
+
+  // Load the index.html
+  mainWindow.loadURL(indexPath)
+    .then(() => {
+      console.log('Window loaded successfully');
+    })
+    .catch((err) => {
+      console.error('Failed to load window:', err);
+      // Try alternative path if initial load fails
+      const altPath = `file://${path.join(process.resourcesPath, 'app/build/index.html')}`;
+      console.log('Trying alternative path:', altPath);
+      return mainWindow.loadURL(altPath);
+    });
+
+  // Open DevTools in development
   if (isDev) {
     mainWindow.webContents.openDevTools();
   }
+
+  // Log loading errors
+  mainWindow.webContents.on('did-fail-load', (event, errorCode, errorDescription) => {
+    console.error('Page load failed:', errorCode, errorDescription);
+  });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
 
-// Quit when all windows are closed.
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
-  }
-});
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
   }
 });
